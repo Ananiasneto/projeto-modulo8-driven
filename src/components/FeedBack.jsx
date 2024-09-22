881283b9c356c868327273637b1c66173e5016e6
Ananias <netim.cunha@gmail.com>
2024-09-21 21:15:50 -0300

    tive um problema ,a estilização acabou dificultando tudo .apaguei e vou recomeçar amanha

---
+++ src/components/FeedBack.jsx
@@ -0,0 +1,17 @@
+import icone_certo from "..//assets/icone_certo.png";
+import icone_errado from "../assets/icone_erro.png";
+import icone_quase from "../assets/icone_quase.png";
+
+export default function Feedback(){
+    return (
+    
+     <>
+    <div className="feedBack">
+        <div><button>nao lembrei</button></div>
+        <div><button>quase nao lembrei</button></div>
+        <div><button>zap</button></div>
+    </div>
+      
+     </> 
+     )
+ }
--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import playImage from '../assets/seta_play.png'
+import imagemVirar from '../assets/seta_virar.png'
+import Feedback from './FeedBack';
+
 
 export default function Flashcard({pergunta,resposta,index}){
-    return (
-    <>
-    <div className="pergunta">
-        <h1>pergunta {index}</h1>
-        
-        <button>
-            <img src={playImage} alt="" />
-        </button>
-        
-    </div>
-     
-    </> 
+   const [questao,setQuestao]=useState('pergunta ' +(index+1)); 
+   const [imagem,setImagem]=useState(playImage);
+   const [respondida,setRespondida]=useState(false);
+    
+   function selecionado(){
+    setQuestao(pergunta);
+    setImagem(imagemVirar);
+   }
+   function pedeFeedBack(){
+    setQuestao(resposta);
+    setRespondida(true);
+   }
+   
+   return (
+    
+   <></> 
     )
 }
-//tem seu feeedbacks
+
+
+      
