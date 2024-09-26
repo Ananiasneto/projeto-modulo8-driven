440ebad2da7ecb0d49d51b7df3e1544208124246
Ananias <netim.cunha@gmail.com>
2024-09-25 22:50:31 -0300

    estilizaçao usando styled components

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import icone_certo from "..//assets/icone_certo.png";
 import icone_errado from "../assets/icone_erro.png";
 import icone_quase from "../assets/icone_quase.png";
 
-export default function Feedback(){
+export default function Feedback({index,setQuestao,setControle}){
+    
+    const[imagemFeed,setImagemFeed]=useState('');
+    const[resposta,setResposta]=useState(false);
+    function selecionada(imagem){
+        setImagemFeed(imagem);
+        setResposta(true);
+        respostaFeed();
+    }
+    function respostaFeed(){
+        setQuestao('pergunta ' +(index+1))
+        setControle(true);
+    }
+
     return (
     
-     <>
-    <div className="feedBack">
-        <div><button>nao lembrei</button></div>
-        <div><button>quase nao lembrei</button></div>
-        <div><button>zap</button></div>
+     <><div>
+        <img src={imagemFeed} alt="" />
+     </div>
+    <div className={resposta ? 'feed': 'feedBack'}>
+        <div className= "errado"><button onClick={()=>selecionada(icone_errado)}>não lembrei </button></div>
+        <div className="quase"><button onClick={()=>selecionada(icone_quase)}>quase não lembrei</button></div>
+        <div className="certo"><button onClick={()=>selecionada(icone_certo)}>zap</button></div>
     </div>
       
      </> 
--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -4,11 +4,13 @@ import imagemVirar from '../assets/seta_virar.png'
 import Feedback from './FeedBack';
 
 
+
 export default function Flashcard({pergunta,resposta,index}){
    const [questao,setQuestao]=useState('pergunta ' +(index+1)); 
    const [imagem,setImagem]=useState(playImage);
    const [respondida,setRespondida]=useState(false);
-    
+   const [controle,setControle]=useState(false);
+
    function selecionado(){
     setQuestao(pergunta);
     setImagem(imagemVirar);
@@ -17,12 +19,26 @@ export default function Flashcard({pergunta,resposta,index}){
     setQuestao(resposta);
     setRespondida(true);
    }
+
    
    return (
     
-   <></> 
+   <>
+<div className={`pergunta ${!controle ? (imagem === playImage ? '' : 'selecionada') + ' ' + (respondida ? 'final' : '') : 'finalizada'}`}>
+
+
+    <h1 >{questao}</h1>
+    <button onClick={(imagem===playImage) ? selecionado : pedeFeedBack} className={(respondida) ? 'botao feed' : 'botao'}>
+        <img className="imgBotao" src={imagem} alt="" />
+       
+    </button>
+    <div className={(!respondida) ? 'feed' : ''}>
+        <Feedback index={index} setQuestao={setQuestao} setControle={setControle}/>
+    </div>
+   </div>
+   </> 
     )
 }
-
+// className={`${imagemFeed===icone_errado ?'escolhida' : ''}`}
 
       
